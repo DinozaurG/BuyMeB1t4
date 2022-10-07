b1513c2b8ec004ee8f5a5e7574cc42cffb0ce3e1
DinozaurG <49711433+DinozaurG@users.noreply.github.com>
2022-10-07 19:48:47 +0700

    Added placeholder on main and some style on modal

--- BuyMeB1t4/ContentView.swift
+++ BuyMeB1t4/ContentView.swift
@@ -19,12 +19,16 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             List {
-                ForEach(items) { item in
-                label: do {
-                    Text("\(item.name!), \(item.count), \(item.counttype!)")
+                if items.count != 0 {
+                    ForEach(items) { item in
+                    label: do {
+                        Text("\(item.name!), \(item.count), \(item.counttype!)")
                     }
+                    }
+                    .onDelete(perform: deleteItems)
+                } else {
+                    Text("Nothing to do here... Press Plus to add product to buy").multilineTextAlignment(.center)
                 }
-                .onDelete(perform: deleteItems)
             }
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
@@ -61,6 +65,16 @@ struct ContentView: View {
     }
 }
 
+struct OvalTextFieldStyle: TextFieldStyle {
+    func _body(configuration: TextField<Self._Label>) -> some View {
+        configuration
+            .padding(15)
+            .background(LinearGradient(gradient: Gradient(colors: [Color.green, Color.blue]), startPoint: .topLeading, endPoint: .bottomTrailing))
+            .cornerRadius(10)
+            .shadow(color: .gray, radius: 10)
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
@@ -75,26 +89,34 @@ struct ModalView: View {
     @State private var counttype: String = ""
     var body: some View {
         VStack {
-            Text("Введите данные о покупке")
-                .padding()
-            TextField("Наименование", text: $name)
-            TextField("Количество", text: $count)
-            TextField("КГ, ШТ?", text: $counttype)
-            Button("Сохранить") {
-                let newItem = Item(context: viewContext)
-                newItem.name = name
-                newItem.count = Int64(count) ?? 1
-                newItem.counttype = counttype
-                do {
-                    try viewContext.save()
-                } catch {
-                    // Replace this implementation with code to handle the error appropriately.
-                    // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                    let nsError = error as NSError
-                    fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-                }
-                self.presentationMode.wrappedValue.dismiss()
-            }
+            VStack {
+                Text("Введите данные о покупке")
+                Divider()
+                VStack {
+                    TextField("Наименование", text: $name)
+                    TextField("Количество", text: $count).keyboardType(.numbersAndPunctuation)
+                    TextField("КГ, ШТ?", text: $counttype)
+                }.foregroundColor(.black)
+                .textFieldStyle(OvalTextFieldStyle())
+                Divider()
+                Button("Сохранить") {
+                    let newItem = Item(context: viewContext)
+                    newItem.name = name
+                    newItem.count = Int64(count) ?? 1
+                    newItem.counttype = counttype
+                    do {
+                        try viewContext.save()
+                    } catch {
+                        // Replace this implementation with code to handle the error appropriately.
+                        // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                        let nsError = error as NSError
+                        fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+                    }
+                    self.presentationMode.wrappedValue.dismiss()
+                }.padding(15)
+                .background(LinearGradient(gradient: Gradient(colors: [Color.red, Color.yellow]), startPoint: .topLeading, endPoint: .bottomTrailing))
+                .cornerRadius(40)
+            }.padding()
         }
     }
 }
